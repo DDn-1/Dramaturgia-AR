466770e0e677cacdc3b5f3738cc267cd0d9ec3cd
DDn-1 <67132474+DDn-1@users.noreply.github.com>
2023-09-19 22:14:50 -0400

    Update gravityMarkers.js

--- javascript/gravityMarkers.js
+++ javascript/gravityMarkers.js
@@ -1,4 +1,4 @@
-
+const loader = new FontLoader();
 let  desktopCube, socket;
 let clock = new THREE.Clock()
 let trackableImages = new Array(9)
@@ -38,16 +38,19 @@ for(let planet in planets){
 	let planetName = planets[planet]
 	let el = document.createElement('div')
 	el.id = 'qr' + planet
-	let geometry = new THREE.TextGeometry( 'Hello three.js!', {
-		font: font,
-		size: 0.1,
-		height: 0.01,
-		curveSegments: 12,
-		bevelEnabled: true,
-		bevelThickness: 10,
-		bevelSize: 8,
-		bevelOffset: 0,
-		bevelSegments: 5
+	loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+	
+		const geometry = new TextGeometry( 'Hello three.js!', {
+			font: font,
+			size: 80,
+			height: 5,
+			curveSegments: 12,
+			bevelEnabled: true,
+			bevelThickness: 10,
+			bevelSize: 8,
+			bevelOffset: 0,
+			bevelSegments: 5
+		} );
 	} );
 	let material = new THREE.MeshBasicMaterial( {color: 0x00ff00} ); 
 	//let geometry = new THREE.SphereGeometry( 0.05, 32, 16 );
