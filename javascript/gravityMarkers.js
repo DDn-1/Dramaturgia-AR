dc8847fc5362ab95e4f747d56ef622d02e64f6ca
DDn-1 <67132474+DDn-1@users.noreply.github.com>
2025-10-31 14:35:46 -0400

    Update gravityMarkers.js

--- javascript/gravityMarkers.js
+++ javascript/gravityMarkers.js
@@ -79,10 +79,9 @@ fontLoader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.
 
             let textMaterial = new THREE.MeshStandardMaterial({ color: planetColors[i] });
             let textMesh = new THREE.Mesh(lineGeometry, textMaterial);
-            textMesh.rotation.x = -Math.PI / 2;
 
             // Posicionar las líneas una debajo de otra
-            textMesh.position.z = -(j * lineSpacing);
+            textMesh.position.z = (j * lineSpacing);
             textMesh.rotation.x = -Math.PI / 2;
 
             textGroup.add(textMesh);
